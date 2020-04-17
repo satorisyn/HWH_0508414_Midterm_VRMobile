470122b2fdc55451d4fb0b157d52d962ba1c1057
satorisyn <satorisyn@users.noreply.github.com>
2020-04-17 17:56:28 +0800

    華夏_賴凱逢_期中_VR手機

---
+++ 0508414_VRMoblie/Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("燈光群組")]
+    public GameObject groupLight;
+    [Header("會動的奧克")]
+    public Transform Box;
+
+
+    public IEnumerator LightEffect()
+    {
+        for (int d = 0; d < 100000; d++)
+        {
+            groupLight.SetActive(false);
+            yield return new WaitForSeconds(0.2f);
+            groupLight.SetActive(true);
+            yield return new WaitForSeconds(0.3f);
+            groupLight.SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+            groupLight.SetActive(true);
+            yield return new WaitForSeconds(0.2f);
+            groupLight.SetActive(false);
+            yield return new WaitForSeconds(0.15f);
+            groupLight.SetActive(true);
+        }
+    }
+    public void StartMoveBox()
+    {
+        StartCoroutine(MoveBox());
+    }
+    public IEnumerator MoveBox()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Box.position += Box.forward * 0.5f;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        Box.GetComponent<CapsuleCollider>().enabled = false;
+    }
+
+    void Start()
+    {
+        StartCoroutine(LightEffect());
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HWH_0508414_Midterm_VRMobile
+ HWH_賴凱逢_期中_VR手機
